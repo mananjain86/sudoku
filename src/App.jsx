0fbfcee0e276d31923eecda308fb4727c0090d14
Manan Jain <jainmanan707@gmail.com>
2025-04-20 01:40:55 +0530

    user inputs in empty cells

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+import { useState } from "react";
 function App() {
-  const board = [
+  const startingBoard = [
     ['5', '3', '', '', '7', '', '', '', ''],
     ['6', '', '', '1', '9', '5', '', '', ''],
     ['', '9', '8', '', '', '', '', '6', ''],
@@ -10,6 +11,16 @@ function App() {
     ['', '', '', '4', '1', '9', '', '', '5'],
     ['', '', '', '', '8', '', '', '7', '9']
   ];
+  const [board, setBoard] = useState(startingBoard);
+
+  const handleChange = (rowIndex, columnIndex, value) => {
+        if (value == '' || /^[1-9]$/.test(value)) {
+          const updatedBoard = board.map((row, r) =>
+            row.map((cell, c) => (r == rowIndex && c == columnIndex ? value : cell))
+          );
+          setBoard(updatedBoard);
+        }
+      };
 
   return(
     <div className="min-h-screen bg-slate-300 flex flex-col p-6">
@@ -19,13 +30,17 @@ function App() {
           row.map((cell, columnIndex) => {
           const topBorder = rowIndex % 3 == 0;
           const leftBorder = columnIndex % 3 == 0;
+          const isFixed = startingBoard[rowIndex][columnIndex] != '';
 
           return(
             <div key={`${rowIndex}-${columnIndex}`} className={`w-16 h-16 text-3xl font-medium text-gray-800 text-center leading-[60px] border border-gray-300
             ${topBorder ? 'border-t-2 border-t-gray-800' : ''}
              ${leftBorder ? 'border-l-2 border-l-gray-800' : ''}
              bg-white`}>
-              {cell}
+            {isFixed ? (<div className="text-gray-600">{cell}</div>) : (
+              <input type="text" value={cell} maxLength={1} onChange={(e) => handleChange(rowIndex, columnIndex, e.target.value)} className="w-full h-full text-center outline-none"
+              />
+            )}
              </div>
           )
         }))}
